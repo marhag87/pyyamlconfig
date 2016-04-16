94e0e067121fb6a54230759b534fea8a6fc360ba
Martin Hagstrom <marhag87@gmail.com>
2016-04-16 10:54:47 +0200

    Add init file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name='pyyamlconfig',
       author='marhag87',
       url='https://github.com/marhag87/pyyamlconfig',
-      version='0.1.5',
+      version='0.1.6',
       packages=find_packages(),
       license='WTFPL',
       description='Load configuration file in yaml format',
