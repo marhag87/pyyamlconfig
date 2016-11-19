ae8b38f8dcf9b4d0992a20c66ad1977a980c9667
Martin Hagstrom <marhag87@gmail.com>
2016-11-19 08:40:24 +0100

    Release v0.2.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/bin/env python
-"""S
-etuptools file for pyyamlconfig
+"""
+Setuptools file for pyyamlconfig
 """
 from setuptools import (
     setup,
@@ -12,7 +12,7 @@ setup(
     author='marhag87',
     author_email='marhag87@gmail.com',
     url='https://github.com/marhag87/pyyamlconfig',
-    version='0.1.10',
+    version='0.2.0',
     packages=find_packages(),
     license='WTFPL',
     description='Load configuration file in yaml format',
